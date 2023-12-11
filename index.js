14a4a71d450fb70195da67ecdc679049b6ae3e68
Abrar <hossainabrar@aol.com>
2023-12-10 23:38:11 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record){
+    const winnings = record.find(function(theYear){
+        return theYear.result === "W"
+    });
+    if(winnings){
+        return winnings.year
+    }else{
+        return undefined
+    }
+};
